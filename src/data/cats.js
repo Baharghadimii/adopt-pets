73a69145efab5f5c3770e736292caa3f09a7954a
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-01-30 23:19:08 -0800

    add meta data about the cats

---
+++ src/data/cats.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    name: 'Fish',
+    breed: 'toxedo',
+    gender: 'male',
+    age: 20,
+    color: 'black/white',
+    weight: 14,
+    location: 'fourside',
+    notes: 'Sweet kitty, He does getting his belly rubbed!'
+  },
+  {
+    name: 'Henry',
+    breed: 'tabby',
+    gender: 'male',
+    age: 20,
+    color: 'orange/white',
+    weight: 17,
+    location: 'threed',
+    notes: 'Super friendly'
+  },
+  {
+    name: 'Roger',
+    breed: 'tabby',
+    gender: 'male',
+    age: 20,
+    color: 'gray',
+    weight: 15,
+    location: 'threed',
+    notes: 'Super friendly'
+  },
+  {
+    name: 'Kitkat',
+    breed: 'bombay',
+    gender: 'female',
+    age: 9,
+    color: 'black',
+    weight: 9,
+    location: 'threed',
+    notes: 'Super friendly'
+  }
+]
